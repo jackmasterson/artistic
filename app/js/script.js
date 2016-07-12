c9a61497f29c4a15d0c7246a189c43c304025f42
JackMasterson <jackmasterson5@gmail.com>
2016-07-11 20:36:53 -0400

    Add header, more paintings

--- app/js/script.js
+++ app/js/script.js
@@ -12,10 +12,36 @@ var model = {
 			src: "img/friendship.jpg",
 			classed: "friendship-portrait",
 			headClass: "friendship-head"
+		},		
+		{
+			name: "Field",
+			src: "img/field.jpg",
+			classed: "field-portrait",
+			headClass: "field-head"
+		},		{
+			name: "Ship",
+			src: "img/ship.jpg",
+			classed: "ship-portrait",
+			headClass: "ship-head"
+		},		{
+			name: "Flowers",
+			src: "img/flowers.jpg",
+			classed: "flowers-portrait",
+			headClass: "flowers-head"
+		},		{
+			name: "Stretch",
+			src: "img/stretch.jpg",
+			classed: "stretch-portrait",
+			headClass: "stretch-head"
 		}
 	]
 };
 
+//for width of pictures/shades, do it as a vw of the number of paintings that
+//there are total; make it an array, then do array.length/x;
+
+//ORRRRR do it by dimensions; assign a height and length to each item in the
+//model when you give it a class/name/etc, and divide it by that
 var viewModel = {
 
 	init: function() {
@@ -33,7 +59,7 @@ var showPaintings = {
 		this.jHeadClass = "."+this.headClass;
 		//console.log(jClass);
 
-		$(this.jHeadClass).slideUp(function() {
+		$(this.jHeadClass).fadeOut(function() {
 			
 			$(that.jClass).animate({
 				opacity: 1
